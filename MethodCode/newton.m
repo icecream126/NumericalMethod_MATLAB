f52f8624f7b0493c08178052a47d95c521445d61
Hyomin Kim <kim123hyomin@gmail.com>
2020-05-24 10:30:44 +0900

    수업진행 후 newton raphson코드 수정

--- MethodCode/newton.m
+++ MethodCode/newton.m
@@ -4,16 +4,8 @@ function root = newton(func1, func2, x0, threshold)
 
 while(1)
     x = x0-feval(func1,x0)/feval(func2,x0);
-    %disp(x0);disp(x);
     rel_error=abs((x-x0)/x)*100;
-    %disp('다음 x는');
-    %disp(x);
-    %disp(abs((x-x0)/x));
-    %disp(abs(x-x0)/x*10);
-    %disp('상대오차는');
     disp(rel_error);
-    x0=x;%xold 값을 다시 갱신
-    
     if(rel_error<threshold)
         break;
     end
